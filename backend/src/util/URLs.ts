240ab59187b794b1bf9d01533f39a73a251481d3
misnawk <kms13011@naver.com>
2024-10-28 18:10:36 +0900

    주소 수정

--- backend/src/util/URLs.ts
+++ backend/src/util/URLs.ts
@@ -1,4 +1,4 @@
 export const SERVER_PORT = 8000;
 export const LOCAL_URL = 'http://localhost:8000';
 export const SERVER_URL =
-  'https://whiteclinic-server-d4xcibm6r-deployproject.vercel.app/';
+  'https://new-back-r6d0mco51-misnawks-projects.vercel.app/';
