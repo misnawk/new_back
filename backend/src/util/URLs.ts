7180de5d265b0588988a3687161567907cd903d9
misnawk <kms13011@naver.com>
2024-10-29 02:05:29 +0900

    2:05

--- backend/src/util/URLs.ts
+++ backend/src/util/URLs.ts
@@ -1,3 +1,3 @@
 export const SERVER_PORT = 8000;
 export const LOCAL_URL = 'http://localhost:8000';
-export const SERVER_URL = 'new-back-i9wfq13d7-misnawks-projects.vercel.app/';
+export const SERVER_URL = 'new-back-2ycyhr5ys-misnawks-projects.vercel.app';
